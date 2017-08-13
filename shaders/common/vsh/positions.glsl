bd6f50dc037a21ef8615372beab15e2ceeb0cbd3
Chris <Nividica@gmail.com>
2017-08-13 17:09:54 -0400

    Turns out gbufferModelViewInverse was required for world position.

--- shaders/common/vsh/positions.glsl
+++ shaders/common/vsh/positions.glsl
@@ -11,15 +11,14 @@
   // WorldPosition
   //   Position where the world coord (0,0,0) is the origin.
   // Requires:
-  //   uniform vec3 cameraPosition;
+  //  uniform vec3 cameraPosition;
+  //  uniform mat4 gbufferModelViewInverse;
   // Arguments:
   //   relativePosition: Relative Position, can be calculated with the RelativePosition macro.
-  #define WorldPosition(relativePosition) (relativePosition.xyz + cameraPosition.xyz)
+  #define WorldPosition(relativePosition) ((gbufferModelViewInverse * relativePosition).xyz + cameraPosition.xyz)
 
   // ClipPosition
   //   Position in the clipping space. This will most commonly be used to set gl_Position.
-  // Requires:
-  //   uniform mat4 gbufferModelView;
   // Arguments:
   //   relativePosition: Relative Position, can be calculated with the RelativePosition macro.
   #define ClipPosition(relativePosition) (gl_ProjectionMatrix * relativePosition)
