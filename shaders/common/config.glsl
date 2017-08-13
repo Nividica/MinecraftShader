081f09f0ead406ad82fe792d510ff9635de887fa
Chris <Nividica@gmail.com>
2017-08-13 14:58:35 -0400

    Added flat-zone to world config

--- shaders/common/config.glsl
+++ shaders/common/config.glsl
@@ -3,4 +3,5 @@
 #define FOG_DISTANCE 1.0 // [0.125 0.25 0.5 1.0 1.25 1.5 1.75 2.0]
 
 #define WORLD_CURVATURE // Enable world curvature
-#define WORLD_CURVATURE_AMOUNT 0 // [0 1 2 3] 0: Minimal 3: Extreme
+  #define WC_AMOUNT 1 // [0 1 2 3] World curvature amount. 0: Minimal 3: Extreme
+  #define WC_FLAT_ZONE 400 //[0 200 400 800 1600] Area around the player that will not be world curved.
--- shaders/common/vsh/world_curvature.glsl
+++ shaders/common/vsh/world_curvature.glsl
@@ -3,15 +3,16 @@
 
 uniform float curve_strengths[ 4 ] = {-4096, -2048, -1024, -512};
 
-float HorizontalDistance(vec4 position){
+float HorizontalDistance(vec3 position){
   return (position.x * position.x) + (position.z * position.z);
 }
 
-void ApplyVerticalCurvature(inout vec4 position, float distanceFromCenter, float reduction){
+void ApplyVerticalCurvature(inout vec3 position, float distanceFromCenter, float reduction){
   position.y += distanceFromCenter / reduction;
 }
 
-void ApplyWorldCurvature(inout vec4 positionRelativeToCamera){
-  float strength = curve_strengths[WORLD_CURVATURE_AMOUNT];
-  ApplyVerticalCurvature(positionRelativeToCamera, HorizontalDistance(positionRelativeToCamera), strength);
+void ApplyWorldCurvature(inout vec3 worldPosition, in vec4 relativePosition){
+  float strength = curve_strengths[WC_AMOUNT];
+  float distanceH = max(HorizontalDistance(relativePosition.xyz) - WC_FLAT_ZONE, 0);
+  ApplyVerticalCurvature(worldPosition, distanceH, strength);
 }
